9d0125402b63716b5c50772264bc902c5dc8fb41
SH1SH4 <dshisha0@gmail.com>
2020-11-11 11:34:42 +0300

    починил

--- проект1/hw_bot.py
+++ проект1/hw_bot.py
@@ -9,38 +9,6 @@ WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
         'Sunday']
 
 
-class Popup(QDialog):  # авторизация всплывающим окном
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        with open('authorize.csv', encoding="utf8") as csvfile:
-            self.reader = \
-                list(csv.DictReader(csvfile, delimiter=';', quotechar='"'))[0]
-            self.id = self.reader['club_id']
-            self.token = self.reader['token']
-        print(self.id, self.token)
-        uic.loadUi('untitled.ui', self)
-        self.setModal(True)
-        self.OkButton.clicked.connect(self.button_ok)
-        self.CancelButton.clicked.connect(self.button_cancel)
-        self.EditToken.setText(self.token)
-        self.EditId.setText(self.id)
-
-    def button_ok(self):
-        self.token = self.EditToken.text()
-        self.id = self.EditId.text()
-        with open('authorize.csv', 'w', newline='') as f:
-            writer = csv.DictWriter(
-                f, fieldnames=['club_id', 'token'],
-                delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
-            writer.writeheader()
-            data = {'club_id': self.id, 'token': self.token}
-            writer.writerow(data)
-        self.close()
-
-    def button_cancel(self):
-        self.close()
-
-
 class MyWidget(QMainWindow):
     def __init__(self):
         self.current_lesson = None
@@ -50,10 +18,28 @@ class MyWidget(QMainWindow):
         self.cur = self.con.cursor()
         self.add_lesson.clicked.connect(self.add_less)
         self.db_update.clicked.connect(self.update_db)
+        self.add_time_message.clicked.connect(self.add_message_time)
         self.lessons_list.itemClicked.connect(self.cur_lesson)
         self.timetable.itemClicked.connect(self.check_homework)
-        self.login = Popup(self)
-        self.authorize.clicked.connect(self.authorizing)
+        self.login_button.clicked.connect(self.ok_login_button)
+        try:
+            with open('authorize.csv', encoding="utf8") as csvfile:
+                self.reader = \
+                    csv.DictReader(csvfile, delimiter=';', quotechar='"')
+                self.reader = list(self.reader)[0]
+                self.id = self.reader['club_id']
+                self.token = self.reader['token']
+                if 'peer_id' in self.reader:
+                    self.peer_id = self.reader['peer_id']
+                else:
+                    self.peer_id = 0
+        except:
+            self.id = ''
+            self.token = ''
+            self.peer_id = 0
+        self.EditToken.setText(self.token)
+        self.EditId.setText(self.id)
+        self.add_dz_button.clicked.connect(self.dz_add_button)
         self.result = self.cur.execute(
             """SELECT lesson FROM lessons""").fetchall()
         self.timetable_ss()
@@ -75,9 +61,17 @@ class MyWidget(QMainWindow):
         print(self.timetable.currentRow(), self.timetable.currentColumn())
         self.cc = self.timetable.currentColumn()
         self.cr = self.timetable.currentRow()
-        self.cur.execute('''SELECT ?
-FROM homework
-WHERE id = ?''', (WEEK[self.cc], self.cr - 1))
+        result = self.cur.execute(f'''SELECT {WEEK[self.cc]}
+FROM homrwork
+WHERE id = {self.cr + 1}''').fetchall()
+        result_lesson = self.cur.execute(f'''SELECT {WEEK[self.cc]}
+FROM timetable_lessons
+WHERE id = {self.cr + 1}''').fetchall()
+        self.result_dz = result[0][0]
+        result_lesson = result_lesson[0][0]
+
+        self.dz_widget.setItem(0, 0, QTableWidgetItem(result_lesson))
+        self.dz_widget.setItem(0, 1, QTableWidgetItem(self.result_dz))
 
     def update_db(
             self):  # вот это должно добавлять выбранный урок и время в выбранную ячейку в таблице
@@ -120,8 +114,49 @@ WHERE id = {self.cur_row + 1}''')
                     self.timetable.setItem(i, j - 1, QTableWidgetItem(
                         str(val) + '  ' + str(self.time)))
 
-    def authorizing(self):  # кнопка авторизации
-        self.login.show()
+    def ok_login_button(self):  # авторизация
+        with open('authorize.csv', encoding="utf8") as csvfile:
+            self.reader = \
+                list(csv.DictReader(csvfile, delimiter=';', quotechar='"'))[0]
+            self.id = self.reader['club_id']
+            self.token = self.reader['token']
+        print(self.id, self.token)
+        self.token = self.EditToken.text()
+        self.id = self.EditId.text()
+        self.EditToken.setText(self.token)
+        self.EditId.setText(self.id)
+        if len(self.id) >= 3 and len(self.token) >= 3:  # проверка на символы(beta)
+            with open('authorize.csv', 'w', newline='', encoding='utf=8') as f:
+                writer = csv.DictWriter(
+                    f, fieldnames=['club_id', 'token'],
+                    delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+                writer.writeheader()
+                data = {'club_id': self.id, 'token': self.token}
+                writer.writerow(data)
+                self.EditToken.setStyleSheet("background-color: white; color: black")
+                self.EditId.setStyleSheet("background-color: white; color: black")
+                self.EditId.setText('ok')
+                self.EditToken.setText('ok')
+        else:
+            if len(self.id) != 9:
+                self.EditId.setText('Некорректный ID')
+                self.EditId.setStyleSheet("color: red")
+            if len(self.token) != 85:
+                self.EditToken.setText('Некорректный Token')
+                self.EditToken.setStyleSheet("color: red")
+
+    def add_message_time(self):
+        print(1)
+
+    def dz_add_button(self):
+        self.cur.execute(f'''UPDATE homrwork
+SET {WEEK[self.cc]} = '{self.dz_widget.item(0, 1).text()}
+WHERE id = {self.cr + 1}''')
+        self.con.commit()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
